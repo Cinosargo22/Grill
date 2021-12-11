ea21067efd23acae99d3a887323a3733e42ec3c2
deluus polycarp <pdeluus@gmail.com>
2021-12-11 13:26:04 -0800

    projet - start

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-fetch('https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=bb917b29&app_key=ca9b61b9c9cb28f8e5aeccd56a855a75').then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-})
+var searchPanel = $('#search-panel');
+var resultsPanel = $('#results-panel');
+var triviaPanel = $('#trivia-panel');
 
-fetch('https://jservice.io/api/random').then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-})
+
+
+
+
+
+
+
+function getRecipes(){
+
+    fetch('https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=bb917b29&app_key=ca9b61b9c9cb28f8e5aeccd56a855a75').then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+function getQuestions(){
+    fetch('https://jservice.io/api/random').then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
